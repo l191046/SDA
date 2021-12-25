347e70a1879254aa70a2d4339c1b236faf078a66
l191046 <82677562+l191046@users.noreply.github.com>
2021-12-25 23:51:42 +0500

    Updated .sql file

--- Airline.sql
+++ Airline.sql
@@ -1,74 +1,123 @@
-CREATE DATABASE Airline
-
+USE master
+GO
+drop database SDA
+GO
+CREATE DATABASE SDA
+GO
+USE SDA
 GO
 
-USE Airline
-
-GO
- 
-CREATE TABLE Person(
-	[Firstname] [varchar](30)  NULL,
-	[LastName] [varchar](30) NULL,
-	[CNIC] [char] (13) NOT NULL,
-	[Address] [varchar] (80) NULL,
-) 
+CREATE TABLE [Person] (
+  [FirstName] nvarchar(255),
+  [LastName] nvarchar(255),
+  [CNIC] varchar(13) NOT NULL,
+  [Address] nvarchar(255)
+)
 GO
 ALTER TABLE Person
 ADD CONSTRAINT PK_Person PRIMARY KEY (CNIC);
-INSERT Person([Firstname], [LastName], [CNIC], [Address]) VALUES ('Sukhan','Amir','3453819234532','Johartown,Lahore')
+
+
+
+CREATE TABLE [Customer] (
+  [CNIC] varchar(13) NOT NULL,
+  [Contact] char(11)
+)
+GO
+ALTER TABLE Customer
+ADD CONSTRAINT PK_Customer PRIMARY KEY (CNIC);
+
+CREATE TABLE [Admin] (
+  [CNIC] varchar(13) NOT NULL ,
+  [EmploymentDate] date,
+  [Salary] float,
+  [Username] varchar(30),
+  [Password] varchar(20)
+)
+GO
+ALTER TABLE Admin
+ADD CONSTRAINT PK_Admin PRIMARY KEY (CNIC);
+
+CREATE TABLE [Flight] (
+  [FlightId] varchar(10)  NOT NULL ,
+  [Source] varchar(30) NOT NULL,
+  [Destination] varchar(30),
+  [Duration] varchar(20),
+  [Cost] int,
+  [Status] varchar(20),
+  [Time] datetime
+)
+GO
+ALTER TABLE Flight
+ADD CONSTRAINT PK_Flight PRIMARY KEY (FlightId);
+
+CREATE TABLE [Airport] (
+  [Code] varchar(30) NOT NULL ,
+  [Name] varchar(30),
+  [City] varchar(20),
+  [Country] varchar(20)
+)
+GO
+ALTER TABLE Airport
+ADD CONSTRAINT PK_Airport PRIMARY KEY (Code);
+CREATE TABLE [Flight_Seats]
+(
+[FlightId] varchar(10) NOT NULL,
+[SeatId] int NOT NULL,
+[Status] varchar(30)
+)
+
+ALTER TABLE Flight_Seats
+ADD CONSTRAINT PK_Seats PRIMARY KEY (FlightId,SeatId);
+ALTER TABLE [Flight_Seats] ADD CONSTRAINT [Flighttoseat] FOREIGN KEY ([FlightId]) REFERENCES [Flight] ([FlightID])
+GO
+
+ALTER TABLE [Customer] ADD CONSTRAINT [PersonToCustomerRef] FOREIGN KEY ([CNIC]) REFERENCES [Person] ([CNIC])
+GO
+
+ALTER TABLE [Admin] ADD CONSTRAINT [PersonToAdminRef] FOREIGN KEY ([CNIC]) REFERENCES [Person] ([CNIC])
+GO
+
+INSERT Person([Firstname], [LastName], [CNIC], [Address]) VALUES ('Sukhan','Amir',3453819234532,'Johartown,Lahore')
 INSERT Person([Firstname], [LastName], [CNIC], [Address]) VALUES ('Rana','Muneem','3452815234532','NFC,Lahore')
 INSERT Person([Firstname], [LastName], [CNIC], [Address]) VALUES ('Razi','Ahmed','5930219384320','Johartown,Lahore')
 INSERT Person([Firstname], [LastName], [CNIC], [Address]) VALUES ('Behzad','Khokhar','3029473829134','Wapda Town,Lahore')
 
+INSERT Customer ([CNIC],Contact) VALUES ('3453819234532','29304532910')
+INSERT Customer ([CNIC],Contact) VALUES ('3452815234532','93043219342')
 
-CREATE TABLE Customer(
-[Contact] [char] (11) NULL
-)
-INSERT Customer ([Contact]) VALUES ('29304532910')
-INSERT Customer ([Contact]) VALUES ('93043219342')
+INSERT ADMIN ([CNIC],[Username],[Password],[Salary],[EmploymentDate]) VALUES ('5930219384320','sukhanamir','crunchyroll',54000,'2000-04-20')
+INSERT ADMIN ([CNIC],[Username],[Password],[Salary],[EmploymentDate]) VALUES ('3452815234532','abdulmuneem','dancingfajita',400000,'2020-01-19')
 
-CREATE TABLE ADMIN(
-[Salary] [int] NULL,
-[Employementdate] [date] NULL
-)
-INSERT ADMIN ([Salary],[Employementdate]) VALUES (54000,'2000-04-20')
-INSERT ADMIN ([Salary],[Employementdate]) VALUES (400000,'2020-01-19')
-INSERT ADMIN ([Salary],[Employementdate]) VALUES (705000,'2007-11-01')
+INSERT Flight([FlightId],[Source],[Destination],[Duration],[Cost],[Status],[Time]) VALUES ('LHE23','Lahore','Abu Dhabi','2.5 hours',50000,'Delayed','11:00')
+INSERT Flight([FlightId],[Source],[Destination],[Duration],[Cost],[Status],[Time]) VALUES ('LHE45','Lahore','Paris','2.5 hours',40000,'On time','23:00')
+INSERT Flight([FlightId],[Source],[Destination],[Duration],[Cost],[Status],[Time]) VALUES ('KHI25','Karachi','New York','12 hours',110000,'Cancelled','16:00')
 
-CREATE TABLE FLIGHT(
-[FlightID] [int] NOT NULL,
-[Source] [varchar](30)  NULL,
-[Destination] [varchar](30) NULL,
-[Duration] [varchar] (20) NULL,
-[Cost] [int] NULL,
-[Status] [varchar] (20) NULL,
-[Time] [time] NULL
-)
+INSERT Airport([Code],[Name],[City],[Country]) VALUES ('US1','US International','New York','USA')
+INSERT Airport([Code],[Name],[City],[Country]) VALUES ('PK35','Jinnah International','Lahore','Pakistan')
+INSERT Airport([Code],[Name],[City],[Country]) VALUES ('DO60','Doha International','Doha','UAE')
 
-ALTER TABLE Flight
-ADD CONSTRAINT PK_Fight PRIMARY KEY (FlightID);
+INSERT Flight_Seats ([FlightId],[SeatId],[Status]) VALUES ('LHE23',30,'Vaccant')
+INSERT Flight_Seats ([FlightId],[SeatId],[Status]) VALUES ('KHI25',45,'Taken')
 
-INSERT FLIGHT([FlightID],[Source],[Destination],[Duration],[Cost],[Status],[Time]) VALUES (2,'Lahore','Abu Dhabi','2.5 hours',50000,'Delayed','11:00')
-INSERT FLIGHT([FlightID],[Source],[Destination],[Duration],[Cost],[Status],[Time]) VALUES (1,'Lahore','Paris','2.5 hours',40000,'On time','23:00')
-INSERT FLIGHT([FlightID],[Source],[Destination],[Duration],[Cost],[Status],[Time]) VALUES (3,'Karachi','New York','12 hours',110000,'Cancelled','16:00')
+GO
+CREATE PROCEDURE admin_signin
+@username	varchar(20),
+@password	varchar(20)
+AS
+	SELECT	[Person].FirstName, [Person].LastName, [Person].CNIC,
+			[Person].Address, [Admin].EmploymentDate, [Admin].Salary
+	FROM	[Admin] inner join [Person] on [Admin].CNIC = [Person].CNIC
+	WHERE	[Admin].Username = @username
+GO
 
 
+--drop procedure admin_signin;
+--EXEC admin_signin @username = 'abdulmuneem', @password = 'dancingfajita';
 
-CREATE TABLE Airport(
-[Code] [int] NOT NULL,
-[Name] [varchar] (30) NULL,
-[City] [varchar] (20) NULL,
-[Country] [varchar] (20) NULL
-)
-
-ALTER TABLE Airport
-ADD CONSTRAINT PK_Airport PRIMARY KEY (Code);
-INSERT Airport([Code],[Name],[City],[Country]) VALUES (1,'US International','New York','USA')
-INSERT Airport([Code],[Name],[City],[Country]) VALUES (2,'Jinnah International','Lahore','Pakistan')
-INSERT Airport([Code],[Name],[City],[Country]) VALUES (3,'Doha International','Doha','UAE')
-
-Select * From Person
-Select * From Customer
-Select * From [ADMIN]
-Select * From FLIGHT
-Select * from Airport
+select * from Person;
+select * from Admin;
+select * from Customer;
+select * from Flight;
+select * from Flight_Seats;
+select * from Airport;
