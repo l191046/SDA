4e50bb62564ddb586ab38856717fd3d5ea1f5550
Rana Abdul Muneem <r.abdulmuneem@gmail.com>
2021-12-25 23:37:34 +0500

    Admin Login implemented

--- Airline/src/Business/Airline.java
+++ Airline/src/Business/Airline.java
@@ -10,22 +10,8 @@ public class Airline {
     
     private static MSsql database;
     private static JSystem system;
-    public static void printDemo(){
-        try(ResultSet result = database.demo()){
-            while(result.next()){
-                String row = new String();
-                for(int i=1; i<=4; i++){
-                    row += result.getString(i) + ' ';
-                }
-                System.out.println(row);
-            }
-        }
-        catch(SQLException e){
-             e.printStackTrace();
-        }
-    }
+    
     public static void main(String[] args) {
-        database = new MSsql();
         system = JSystem.getInstance();
         
         JHome jHome = new JHome(system);
--- Airline/src/Database/MSsql.java
+++ Airline/src/Database/MSsql.java
@@ -8,10 +8,19 @@ import java.sql.SQLException;
 
 public class MSsql {
     //CHANGE SERVER NAME ACCORDINGLY AND SAVE
-    protected String url="jdbc:sqlserver://RAMHP\\MSSQLSERVER;"+"databaseName=Airline;integratedSecurity=true;";   //Abdul Muneem
+    protected String url="jdbc:sqlserver://RAMHP\\MSSQLSERVER;"+"databaseName=SDA;integratedSecurity=true;";   //Abdul Muneem
     //protected String url="jdbc:sqlserver://RAMHP\\MSSQLSERVER;"+"databaseName=Airline;integratedSecurity=true;"; //Razi
     //protected String url="jdbc:sqlserver://RAMHP\\MSSQLSERVER;"+"databaseName=Airline;integratedSecurity=true;"; //Sukhan
     //protected String url="jdbc:sqlserver://RAMHP\\MSSQLSERVER;"+"databaseName=Airline;integratedSecurity=true;"; //Behzad
+    
+    private static final MSsql instance = new MSsql();
+    
+    private MSsql(){
+        
+    }
+    public static MSsql getInstance(){
+        return instance;
+    }
     
     public ResultSet demo(){
         ResultSet result = null;
@@ -22,6 +31,25 @@ public class MSsql {
             PreparedStatement pst;
             pst = con.prepareStatement(sql);
             result = pst.executeQuery();
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+    
+    public ResultSet checkAdmin(String username, String password){
+        ResultSet result = null;
+        try {
+            Connection con = DriverManager.getConnection(url);
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            //CALLING STORED PROCEDURE
+            String SQL = "{call [admin_signin](?,?)}";
+            //PROCEDURE PARAMETERS
+            CallableStatement Cmt = con.prepareCall(SQL);
+            Cmt.setString(1, username);
+            Cmt.setString(2, password);
+            result=Cmt.executeQuery();
         }
         catch(Exception e) {
             e.printStackTrace();
